cae9bf1ee5983ca81dbf0d81e8aa5494f76eab29
Harsh Dixit <143201960+harshdixit3@users.noreply.github.com>
2025-03-13 20:04:12 +0530

    completed the nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,7 @@
-import React from "react";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
-import { Button } from "@/components/ui/button";
-
 const Page = () => {
   return (
     <div className="mt-4">
-      <SignedOut>
-        <SignInButton>
-          <Button>Sign In</Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+      <h1>Home page content</h1>
     </div>
   );
 };
